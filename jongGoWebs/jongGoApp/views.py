1f818fb023284adf6f54f73dc4d86dc7ee43e2eb
ntek-4548 <jeces@naver.com>
2021-06-11 17:31:24 +0900

    06_11 YS

--- jongGoWebs/jongGoApp/views.py
+++ jongGoWebs/jongGoApp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 # Create your views here.
 
+
 def index(request):
+    dir = db.reference()  # 기본위치 지정
+    datae_1 = db.reference().child()
     return render(request, 'jongGoApp/index.html')
+
