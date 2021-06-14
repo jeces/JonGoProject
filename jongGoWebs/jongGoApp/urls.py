822561a56f85dfaa61ecabb8c9289aad65ccd7df
dk <kamal1234@naver.com>
2021-06-14 13:34:17 +0900

    06_14

--- jongGoWebs/jongGoApp/urls.py
+++ jongGoWebs/jongGoApp/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from.import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('join/', views.join, name='join'),
     path('login/', views.login, name='login'),
 ]
